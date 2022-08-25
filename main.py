9d387dc9e03c105378f40d7a12325c87f0f1b982
leonardodma <leonardodma@al.insper.edu.br>
2022-08-24 22:25:02 -0300

    refactoring code

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
 import sys
+from utils import *
 
 def main(argv, arc):
-    count_string = argv[1]
-    if count_string.find(" ") > 1:
-        raise TypeError("Invalid Sintax")
-    else:
-        numbers= []
-        signals = []
-        operations = ["+", "-"]
-
-        n = ""
-        for char in count_string:
-            if char not in operations:
-                n += char
-            else:
-                numbers.append(n)
-                signals.append(char)
-                n = ""
-                    
-        if n not in operations and n != "": numbers.append(n)
-
-        if len(numbers) <= len(signals):
-            raise TypeError("Invalid Sintax: the amount of operators are larger than numbers")
-        else:
-            count_result = int(numbers[0])
-            for i in range(len(signals)):
-                if signals[i] == "+":
-                    count_result += int(numbers[i+1])
-                elif signals[i] == "-":
-                    count_result -= int(numbers[i+1])
-
-        print(count_result)
-        return count_result
+    print(Parser.run(argv[1]))
 
 if __name__ == '__main__':
     main(sys.argv, len(sys.argv))
---
+++ utils.py
@@ -0,0 +1,101 @@
+
+from curses.ascii import isdigit
+
+
+class Token():
+    def __init__(self, type: str, value):
+        self.type = type
+        self.value = value
+
+
+class Tokenizer():
+    def __init__(self, source: str):
+        self.source = source.replace(" ", "")
+        self.position = 0
+        self.operations = {"+": "PLUS", "-": "MINUS"}
+
+        value = ""
+        for char in self.source:
+            if char != " ":
+                if value == "" and char in self.operations.keys():
+                    raise ValueError(
+                        "Invalid sintax: your input should not start with an operation")
+                elif value != "" and char in self.operations.keys():
+                    self.next = Token("INT", int(value))
+                    break
+                elif self.position + 1 >= len(self.source):
+                    value += char
+                    self.next = Token("INT", int(value))
+                    break
+                elif char.isdigit():
+                    value += char
+                else:
+                    raise ValueError(f"Invalid sintax: invalid token '{char}'")
+
+            self.position += 1
+
+    def selectNext(self):
+        if self.position >= len(self.source):
+            self.next = Token("EOP", None)
+        else:
+            source = self.source[self.position:]
+            value = ""
+
+            for i in range(len(source)):
+                char = source[i]
+
+                if char != " ":
+                    if char.isdigit() or char in self.operations.keys():
+                        if char.isdigit():
+                            value += char
+                            # End of operation
+                            if i + 1 >= len(source):
+                                self.next = Token("INT", int(value))
+                                self.position += 1
+                                break
+
+                        else:
+                            if value == "":
+                                if self.next.type in self.operations.values():
+                                    raise ValueError(
+                                        "Invalid Sintax: two operators in a row")
+                                elif i + 1 >= len(source):
+                                    raise ValueError(
+                                        "Can not end with an operator")
+                                self.next = Token(self.operations[char], char)
+                                self.position += 1
+
+                            else:
+                                self.next = Token("INT", int(value))
+
+                            break
+                    else:
+                        raise ValueError(
+                            f"Invalid sintax: invalid token '{char}'")
+
+                self.position += 1
+
+
+class Parser():
+    tokenizer = None
+
+    @ staticmethod
+    def parseExpression():
+        total = Parser.tokenizer.next.value
+        while Parser.tokenizer.next.type != "EOP":
+            Parser.tokenizer.selectNext()
+
+            if Parser.tokenizer.next.type == "PLUS": 
+                Parser.tokenizer.selectNext()
+                total += Parser.tokenizer.next.value
+            elif Parser.tokenizer.next.type == "MINUS":
+                Parser.tokenizer.selectNext()
+                total -= Parser.tokenizer.next.value
+
+        return total
+
+    @ staticmethod
+    def run(code):
+        Parser.tokenizer = Tokenizer(code)
+
+        return Parser.parseExpression()
