d94068069cf5aa3dc3ea450bc9a3dab2707069ad
leonardodma <leonardodma@al.insper.edu.br>
2022-11-14 19:53:31 -0300

    changing argv

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main(argv, arc):
     $ nasm -f elf32 -F dwarf -g program.asm
     $ ld -m elf_i386 -o program program.o
     """
-    # filename = argv[1]
-    filename = "code/while.carbon"
+    filename = argv[1]
+    # filename = "code/while.carbon"
     with open(filename, "r") as file:
         source = file.read()
         Parser.run(source)
